c68333d439b31567b3439fd8aade7f1ca1087029
Brian Noh <noh24@ymail.com>
2022-10-28 12:41:39 -0400

    Clear test6

--- js/scripts.js
+++ js/scripts.js
@@ -111,3 +111,29 @@ function beepityBoopity(num) {
   }
   return newAr;
 }
+
+// Test6: Take a string of numbers with two double digits of 2 AND 3.
+// Code: 
+// const num = "3, 0, 2, 1, 13, 23";
+// beepityBoopity(num);
+// Expected Output: ["Won't you be my neighboor?", 0, "Boop", "Beep", "Won't you be my neighboor?", "Won't you be my neighboor?"];
+
+const num = "3, 0, 2, 1, 13, 23";
+
+function beepityBoopity(num) {
+  const numAr = num.split(" ");
+  let newAr = [];
+
+  for (let i = 0; i < numAr.length; i++) {
+    if (numAr[i].includes(3)) {
+      newAr.push("Won't you be my neighboor?");
+    } else if (numAr[i].includes(2)) {
+      newAr.push("Boop");
+    } else if (numAr[i].includes(1)) {
+      newAr.push("Beep");
+    } else {
+      newAr.push(numAr[i]);
+    }
+  }
+  return newAr;
+}
