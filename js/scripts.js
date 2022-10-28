9ce7a811f4009f1c4cd530f27c14dd4bd25c195c
Brian Noh <noh24@ymail.com>
2022-10-28 12:11:07 -0400

    Add function beepityBoopity to clear test1

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// Describe beepityBoopity()
+
+// Test1: Take string 0, return array of 0
+// Code: 
+// const num = "0";
+// beepityBoopity(num);
+// Expected Output: [0];
+
+const num = "0";
+
+function beepityBoopity(num) {
+  let numAr = [];
+  numAr.push(num);
+  return numAr;
+}
