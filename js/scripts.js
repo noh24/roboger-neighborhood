359d7e7aa7a79b9d96178b65033f9201d8b46a49
Brian Noh <noh24@ymail.com>
2022-10-28 19:11:51 -0400

    WIP: Add function to display results

--- js/scripts.js
+++ js/scripts.js
@@ -38,9 +38,19 @@ document.getElementById("fire").addEventListener("click", function() {
   document.querySelector("legend").classList.add("hidden");
 });
 
-document.querySelector("form").addEventListener("submit", function () {
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
   document.querySelector("legend").classList.add("hidden");
+  getInput();
+});
+
+function getInput() {
   const name = document.getElementById("user-name").value;
   const input = document.getElementById("user-num").value;
-  
-});
+  const inputArray = countArray(input);
+  const beepBoop = beepityBoopity(inputArray, name);
+
+  for (let i = 0; i < beepBoop.length; i++) {
+    document.createElement("li").append(beepBoop[i]);
+  }
+}
